a14a194221a83393f8dcdbdea2f8364bd5ac2901
Dnyaneshwari-Dhumal <dnyaneshwari3dhumal@gmail.com>
2024-08-25 23:02:35 +0530

    Patterns 2

---
+++ Practicals/Assi8 (Patterns 2)/pr11.dart
@@ -0,0 +1,18 @@
+import "dart:io";
+
+void main() {
+  int n = int.parse(stdin.readLineSync()!);
+  int num = 0;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      if (j % 2 == 1) {
+        stdout.write("$num");
+        num++;
+      } else {
+        num--;
+        stdout.write("$num");
+      }
+    }
+    print("");
+  }
+}
